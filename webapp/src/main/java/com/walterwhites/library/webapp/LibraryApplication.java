e23ec4c5232579135213272b9ff09abe517253ad
walterwhites <hopemagie@gmail.com>
2018-12-11 03:19:27 +0100

    update libs, Controllers and tests

---
+++ webapp/src/main/java/com/walterwhites/library/webapp/LibraryApplication.java
@@ -0,0 +1,20 @@
+package com.walterwhites.library.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class LibraryApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LibraryApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
