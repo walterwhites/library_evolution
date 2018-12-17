460ebd22b9efcbd492233464ac1ef3701f15e55a
walterwhites <hopemagie@gmail.com>
2018-12-17 03:44:01 +0100

    push on cloud foundry

---
+++ consumer/src/main/java/com/walterwhites/library/consumer/contract/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.walterwhites.library.consumer.contract.repository;
+
+import com.walterwhites.library.model.entity.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+} d
--- webapp/src/main/java/com/walterwhites/library/webapp/LibraryApplication.java
+++ webapp/src/main/java/com/walterwhites/library/webapp/LibraryApplication.java
@@ -2,9 +2,13 @@ package com.walterwhites.library.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.walterwhites.library.consumer.contract.repository")
+@EntityScan("com.walterwhites.library.model.entity")
 @SpringBootApplication
 public class LibraryApplication extends SpringBootServletInitializer {
 
