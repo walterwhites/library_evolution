3414a70751e4123b3c61ecdfdfdbdb6466fadd31
walterwhites <hopemagie@gmail.com>
2019-02-25 11:44:51 +0100

    send email to client and Admin

--- batch_email/src/main/java/com/walterwhites/library/batch_email/processor/LoanItemProcessor.java
+++ batch_email/src/main/java/com/walterwhites/library/batch_email/processor/LoanItemProcessor.java
@@ -16,21 +16,23 @@ public class LoanItemProcessor implements ItemProcessor<Loans, Loans> {
     public EmailService emailService;
 
     @Override
-    public Loans process(Loans item) throws Exception {
-        /*File csv = new File(LoanItemProcessor.class.getClassLoader().getResource("user_email.csv").getFile());
-        CSVWriter writer = new CSVWriter(new FileWriter(csv, true));
-        String str = item.getClient().getEmail() + "," + item.getClient().getFirstname() + " " +
-                item.getClient().getLastname() + "," + item.getBook().getTitle();
-        String[] record = str.split(",");
-        writer.writeNext(record);
-        writer.close();*/
-        log.info("Email sended to " + item.getClient().getEmail());
-        emailService.sendSimpleMessage("hopemagie@gmail.com", "Book loan", "Hello " +
-                        item.getClient().getFirstname() + " " + item.getClient().getLastname() +
-                ", you have borrowed the " + item.getStartDate().getDay() + "/" + item.getStartDate().getMonth() + "/" +
-                item.getStartDate().getYear() + " the book " + item.getBook().getTitle() + ", you had to return it " +
+    public Loans process(Loans item) {
+        String message = "Hello " +
+                item.getClient().getFirstname() + " " + item.getClient().getLastname() +
+                item.getStartDate().getDay() + "/" + item.getStartDate().getMonth() + "/" +
+                item.getStartDate().getYear() + " you have borrowed the book " + item.getBook().getTitle() + ", you had to return it " +
                 "before the " + item.getEndDate().getDay() + "/" + item.getEndDate().getMonth() + "/" + item.getEndDate().getYear() +
-                " please can return it");
+                " please can return it";
+        String message_admin = item.getClient().getFirstname() + " " + item.getClient().getLastname() +
+                " have borrowed the book " + item.getBook().getTitle() + " from " +
+                item.getStartDate().getDay() + "/" + item.getStartDate().getMonth() + "/" +
+                item.getStartDate().getYear() + " to " +
+                item.getEndDate().getDay() + "/" + item.getEndDate().getMonth() + "/" + item.getEndDate().getYear() +
+                " but he had not returned it yet";
+
+        log.info("Email sended to " + item.getClient().getEmail());
+        emailService.sendSimpleMessage("hopemagie@gmail.com", "Client has not returned book", message_admin);
+        emailService.sendSimpleMessage("hopemagie@gmail.com", "You forgot to return a book", message);
 
         return item;
     }
